2fabd4e3fd0a012b8e5c3c01e8c20754ab710911
sasaplus1 <sasaplus1@gmail.com>
2016-07-09 13:58:06 +0900

    added scripts

---
+++ debounce.js
@@ -0,0 +1,83 @@
+/*!
+ * @license debounce.js Copyright(c) 2016 sasa+1
+ * https://github.com/sasaplus1-prototype/debounce.js
+ * Released under the MIT license.
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["debounce"] = factory();
+	else
+		root["debounce"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	module.exports = function(fn, timeout) {
+	  var timer;
+
+	  return function() {
+	    var that = this,
+	        args = arguments;
+
+	    clearTimeout(timer);
+
+	    timer = setTimeout(function() {
+	      fn.apply(that, args);
+	    }, timeout);
+	  };
+	};
+
+
+/***/ }
+/******/ ])
+});
+;
